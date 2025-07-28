3a73ed5c14a77eea22490c3248f84d2800458e8d
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-07-28 08:58:20 +0200

    Add test summary

--- test-utils.scm
+++ test-utils.scm
@@ -11,5 +11,23 @@
           (display "  Got: ") (write actual) (newline)))
     result))
 
+(define (count pred lst)
+  (if (null? lst)
+      0
+      (+ (if (pred (car lst)) 1 0)
+         (count pred (cdr lst))
+      )))
+
+(define (summary)
+  (let* ((total (length test-results))
+         (passed (count (lambda (x) x) test-results))
+         (failed (- total passed)))
+    (newline)
+    (display "Summary:\n")
+    (display "  Total tests: ") (write total) (newline)
+    (display "  Passed: ") (write passed) (newline)
+    (display "  Failed: ") (write failed) (newline)
+    (exit)))
+
 (define (initialize-test-results)
   (set! test-results '()))

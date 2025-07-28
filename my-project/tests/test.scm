6eafa7c70636765501f2140ca1bba56cefac7420
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-07-28 09:47:58 +0200

    Display only summary on test success

--- my-project/tests/test.scm
+++ my-project/tests/test.scm
@@ -4,8 +4,8 @@
 (initialize-test "My-Project test suite")
 
 (assert-equal 4 (plus 2 2))
-(assert-equal 6 (plus 3 2))
-(assert-equal 6 (plus 3 2))
-(assert-equal 6 (plus 3 2))
+(assert-equal 6 (plus 3 3))
+(assert-equal 8 (plus 4 4))
+(assert-equal 10 (plus 5 5))
 
 (end-test)

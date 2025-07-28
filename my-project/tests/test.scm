c6d1793ee0f0b0e7f727c40a2c4d170e8dece8c3
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2025-07-28 08:59:25 +0200

    Add dummy project for testing

---
+++ my-project/tests/test.scm
@@ -0,0 +1,11 @@
+(load "test-utils.scm")
+(load "001/src/logic.scm")
+
+(initialize-test-results)
+
+(define (run-tests)
+  (assert-equal 4 (plus 2 2))
+  (assert-equal 6 (plus 3 2)))
+
+(run-tests)
+(summary)
